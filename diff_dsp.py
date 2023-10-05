6198c7c05758a571b35653a5d41109367bcc64ff
Gloria Dal Santo <gloria.dalsanto@outlook.it>
2023-10-05 14:58:44 +0200

    Add model

--- diff_dsp.py
+++ diff_dsp.py
@@ -1,133 +1,141 @@
-# differentiable delay network 
+# differentiable digital signal processing blocks
+# comput
 import torch 
 import torch.nn as nn
 import torch.nn.functional as F
 from utils.utility import get_device
 
+def SVF(x, f, R, mLP, mBP, mHP):
+    ''' differentiable implementation of iir filter as serialy cascaded 
+    state variable filters (SVRs)
+    x: frequency sampling points
+    f: cut off frequency 
+    R: resonance 
+    mLP, mBP, mHP: mixing coefficents'''
+    K = f.size(1) # number of cascaded filters 
+    bs = f.size(0) # batch size 
+    # compute biquad filter parameters  
+    beta = torch.zeros((bs, 3, K), device=get_device())     
+    alpha = torch.zeros((bs, 3, K), device=get_device())  
 
-class SVF(nn.Module):
-    def __init__(self, K):
-        super().__init__() 
-        ''' differentiable implementation of iir filter as serialy cascaded 
-        state variable filters (SVRs)
-        K: number of filters '''
-        self.K = K
-        self.f = nn.Parameter(torch.randn(1, self.K))
-        self.R = nn.Parameter(torch.randn(1, self.K))
-        self.mLP = nn.Parameter(torch.randn(1, self.K))
-        self.mBP = nn.Parameter(torch.randn(1, self.K))
-        self.mHP = nn.Parameter(torch.randn(1, self.K))
+    beta[:,0,:] = (f**2) * mLP + f * mBP + mHP
+    beta[:,1,:] = 2*(f**2)*mLP - 2*mHP
+    beta[:,2,:] = (f**2) * mLP - f * mBP + mHP
 
-    def forward(self, x):
-        ''' x: sampling points '''
-        # compute biquad filter parameters  
-        beta = torch.zeros((3, self.K), device=get_device())     
-        alpha = torch.zeros((3, self.K), device=get_device())  
-        beta[0,:] = (self.f**2) * self.mLP + \
-            self.f * self.mBP + self.mHP
-        beta[1,:] = 2*(self.f**2)*self.mLP - 2*self.mHP
-        beta[2,:] = (self.f**2) * self.mLP - \
-            self.f * self.mBP + self.mHP
-        alpha[0,:] = self.f**2 + 2*self.R*self.f + 1
-        alpha[1,:] = 2*self.f**2 - 2
-        alpha[2,:] = self.f**2 - 2*self.R*self.f + 1
-        # convert parameters to complex 
-        beta = torch.complex(beta, torch.zeros((3, self.K), device=get_device()))
-        alpha = torch.complex(alpha, torch.zeros((3, self.K), device=get_device()))
-        H = torch.complex(torch.ones(x.size(), device=get_device()),torch.ones(x.size(), device=get_device()))
-        for k in range(self.K):
-            Hi = torch.div(
-                    torch.matmul(
-                        torch.pow(x.expand((3 ,-1)).transpose(1, 0), 
-                        torch.tensor([0, -1, -2], device=get_device())),
-                        beta[:,k]),
-                    torch.matmul(
-                        torch.pow(x.expand((3 ,-1)).transpose(1, 0), 
-                        torch.tensor([0, -1, -2], device=get_device())),
-                        alpha[:,k])
-            )
-            H = H*Hi
-        return H
+    alpha[:,0,:] = f**2 + 2*R*f + 1
+    alpha[:,1,:] = 2*f**2 - 2
+    alpha[:,2,:] = f**2 - 2*R*f + 1
+    # convert parameters to complex 
+    beta = torch.complex(beta, torch.zeros(beta.size(), device=get_device()))
+    alpha = torch.complex(alpha, torch.zeros(alpha.size(), device=get_device()))
 
+    H = biquad_to_tf(x, beta[:,:,0], alpha[:,:,0])
+    for k in range(1, K):
+        Hi = biquad_to_tf(x, beta[:,:,k], alpha[:,:,k])
+        H = torch.mul(H, Hi)  
+    return H 
 
-class SAP(nn.Module):
-    def __init__(self, m):
-        super().__init__()
-        ''' differentiable Schoreder allpass filter 
-            m: delay lengths'''
-        self.m = m 
-        self.K = m.size(0)
+def SAP(x, m, gamma):
+    ''' differentiable Schoreder allpass filter 
+    x: frequency sampling points 
+    m: delay lengths
+    gamma: feed-forward/back gains'''
+    M = m.size(0) # number of channels
+    K = m.size(1) # number of cascaded filters 
+    bs = gamma.size(0) # batch size
+    # compute transfer function of first filter
+    zK = torch.pow(x.expand(M,-1).permute(1, 0),-m[:,0]).expand(bs, -1, -1)     # this step instroudces some numerical errors so that the absolute value is no longer <= 1
+    gammaK = (gamma[:,:,0].expand(x.size(0), -1, -1)).permute(1, 0, 2)
+    H = torch.div(
+        (gammaK + zK), 
+        (1 + gammaK * zK))
+    # compute all the other SAP filters in series 
+    for k in range(1, K):
+        zK = torch.pow(x.expand(M,-1).permute(1, 0),-m[:,k]).expand(bs, -1, -1)
+        gammaK = (gamma[:,:,k].expand(x.size(0), -1, -1)).permute(1, 0, 2)
+        Hi = torch.div(
+            (gammaK + zK),
+            (1 + gammaK * zK))
+        # element-wise mul to compute overall system's transfer function
+        H = torch.mul(H, Hi)    
+    return H 
 
-        self.gamma = nn.Parameter(torch.randn(self.K))
+def PEQ(x, f, R, G):
+    ''' differentiable parameteric equilizer
+    x: frequency sampling points
+    f: cut off frequency 
+    R: resonance 
+    G: component gain
+    f entries at initialization must be in ascending order '''
+    # TODO: for some reason only when coefficents alpha and beta are swapped 
+    # we obtain the proper response
 
-    def forward(self, x):
+    K = f.size(1)   # number of filters in series
+    bs = f.size(0)   # number of channels
+    # prevent the shelf filters magniture response to spkie over 1
+    R[:,0] = R[:,0] + 1/torch.sqrt(torch.tensor(2, device=get_device())) 
+    R[:,-1] = R[:,-1] + 1/torch.sqrt(torch.tensor(2, device=get_device())) 
+    # TODO: it seems that low values of R give better looking respononses (low: close to 1/sqrt(2))
+    # maybe the activation should not push R to high values 
+    # Watch out that the formula used for the  biquad coeff of LP and HP filters are swapped 
+    # low shelf filter (as flipped high shelf)
+    # G[:,0] = G[:,0] - torch.tensor(0.001, device=get_device())   # this is needed to prevent the low freq from being aplifyed
+    betaLP, alphaLP = compute_biquad_coeff(
+        f[:,0], R[:,0], torch.tensor(1, device=get_device()), 2 * R[:,0] * torch.sqrt(G[:,0]), G[:,0] )
+        # f[:,0], R[:,0], G[:,0], 2 * R[:,0] * torch.sqrt(G[:,0]), torch.tensor(1, device=get_device()) )
+    HHP = biquad_to_tf(x, betaLP[:,:,0], alphaLP[:,:,0]) - 1e-5
+    H = HHP 
 
-        H = torch.complex(torch.ones(x.size(), device=get_device()),torch.ones(x.size(), device=get_device()))
-        for k in range(self.K):
-            Hi = torch.div(
-                (1 + self.gamma[k] * x ** (-self.m[k])),
-                (self.gamma[k] + x ** (-self.m[k]))
-            )
-            H = H*Hi
-        return H
+    # high shelf filter (as flipped low shelf)
+    
+    betaHP, alphaHP = compute_biquad_coeff(
+        f[:,-1], R[:,-1], G[:,-1], 2 * R[:,0] * torch.sqrt(G[:,-1]), torch.tensor(1, device=get_device()) )
+        # f[:,-1], R[:,-1], torch.tensor(1, device=get_device()), 2 * R[:,-1] * torch.sqrt(G[:,-1]), G[:,-1] )  
+    HLP = biquad_to_tf(x, betaHP[:,:,0], alphaHP[:,:,0]) - 1e-5
+    H = H*HLP
 
-class PEQ(nn.Module):
-    def __init__(self, K):
-        super().__init__()
-        ''' differentiable parametric equilizer 
-            K: number of filters'''
-        
-        self.K = K
-        self.f = nn.Parameter(torch.randn(self.K))
-        self.R = nn.Parameter(torch.randn(self.K))
-        self.G = nn.Parameter(torch.randn(self.K))
+    # K - 2 peaking filter 
+    for k in range(1, K-1):
+        beta, alpha = compute_biquad_coeff(
+            f[:,k], R[:,k], torch.tensor(1, device=get_device()), 2*R[:,k]*G[:,k], torch.tensor(1, device=get_device()))
+        Hp = biquad_to_tf(x, beta[:,:,0], alpha[:,:,0]) - 1e-5
+        H = H*Hp
+    return H
 
-    def forward(self, x):
-        ''' x: sampling points '''
-
-        H = torch.complex(torch.ones(x.size(), device=get_device()),torch.ones(x.size(), device=get_device()))
-        # low shelf filter 
-        betaLP, alphaLP = self.compute_biquad_coeff(
-            self.f[0], self.R[0], self.G[0], 2 * self.R[0] * torch.sqrt(self.G[0]), 1)  
-        HLP = self.compute_tf(x, betaLP, alphaLP)
-        H = H*HLP
-
-        # high shelf filter 
-        betaHP, alphaHP = self.compute_biquad_coeff(
-            self.f[1], self.R[1], 1, 2 * self.R[1] * torch.sqrt(self.G[1]), self.G[1])
-        HHP = self.compute_tf(x, betaHP, alphaHP)
-        H = H*HHP
-
-        # K - 2 peaking filter 
-        for k in range(2, self.K):
-            beta, alpha = self.compute_biquad_coeff(
-                self.f[k], self.R[k], 1, 2*self.R[k]*self.G[k], 1)
-            Hp = self.compute_tf(x, beta, alpha)
-            H = H*Hp
-        return H
-
-    def compute_biquad_coeff(self, f, R, mLP, mBP, mHP):
-        beta = torch.zeros(3, device=get_device())     
-        alpha = torch.zeros(3, device=get_device())  
-        beta[0] = (f**2) * mLP + f * mBP + mHP
-        beta[1] = 2*(f**2) * mLP - 2 * mHP
-        beta[2] = (f**2) * mLP - f * mBP + mHP
-        alpha[0] = f**2 + 2*R*f + 1
-        alpha[1] = 2* (f**2) - 2
-        alpha[2] = f**2 - 2*R*f + 1  
-        beta = torch.complex(beta, torch.zeros(3, device=get_device()))
-        alpha = torch.complex(alpha, torch.zeros(3, device=get_device()))     
-        return beta, alpha
-
-    def compute_tf(self, x, beta, alpha):
+def biquad_to_tf(x, beta, alpha):
+    # TODO: too many transpose operations. they can be removed
         H = torch.div(
-                    torch.matmul(
-                        torch.pow(x.expand((3 ,-1)).transpose(1, 0), 
+                torch.matmul(
+                    torch.pow(x.expand((3 ,-1)).transpose(1, 0), 
                         torch.tensor([0, -1, -2], device=get_device())),
-                        beta),
-                    torch.matmul(
-                        torch.pow(x.expand((3 ,-1)).transpose(1, 0), 
+                    beta.transpose(1,0)),
+                torch.matmul(
+                    torch.pow(x.expand((3 ,-1)).transpose(1, 0), 
                         torch.tensor([0, -1, -2], device=get_device())),
-                        alpha)
+                    alpha.transpose(1,0))
             )
-        return H
+        return H.transpose(1, 0) 
+
+def compute_biquad_coeff(f, R, mLP, mBP, mHP):
+    if f.dim() == 1:
+        f = torch.unsqueeze(f, dim=-1)
+        R = torch.unsqueeze(R, dim=-1)
+        mLP = torch.unsqueeze(mLP, dim=-1)
+        mBP = torch.unsqueeze(mBP, dim=-1)
+        mHP = torch.unsqueeze(mHP, dim=-1)
+
+    K = f.size(1) # number of cascaded filters
+    bs = f.size(0) # batch size  
+    beta = torch.zeros(bs, 3, K, device=get_device())     
+    alpha = torch.zeros(bs, 3, K, device=get_device())  
+
+    beta[:,0,:] = (f**2) * mLP + f * mBP + mHP
+    beta[:,1,:] = 2*(f**2) * mLP - 2 * mHP
+    beta[:,2,:] = (f**2) * mLP - f * mBP + mHP
+    alpha[:,0,:] = f**2 + 2*R*f + 1
+    alpha[:,1,:] = 2* (f**2) - 2
+    alpha[:,2,:] = f**2 - 2*R*f + 1  
+
+    beta = torch.complex(beta, torch.zeros(beta.size(), device=get_device()))
+    alpha = torch.complex(alpha, torch.zeros(alpha.size(), device=get_device()))
+    return beta, alpha
---
+++ model.py
@@ -0,0 +1,290 @@
+import torch
+import torch.nn as nn 
+import torch.nn.functional as F
+from nnAudio import features
+from diff_dsp import * 
+from utils.processing import householder_matrix as householder
+from utils.utility import get_device
+
+# MODEL/TASK_AGNOSTIC ENCODER 
+class Encoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        ''' model/task agnostic decoder '''
+
+        self.stft = features.stft.STFT(
+            n_fft = 1024,
+            hop_length = 128,
+            window = 'hann',
+            freq_scale = 'log',
+            sr = 44100,
+            output_format = 'Magnitude',
+            verbose=False
+        )
+        self.conv_depth = 5
+        self.chn_out = [64, 128, 128, 128, 128]  
+        self.kernel = [(7,5), (5,5), (5,5), (5,5), (5,5)]
+        self.strides = [(1,2), (2,1), (2,2), (2,2), (1,1)]
+
+        self.conv_list = nn.ModuleList([])
+        for i in range(self.conv_depth):
+            if i == 0:
+                chn_in = 1
+            else:
+                chn_in = self.chn_out[i-1]
+            
+            self.conv_list.append(nn.ModuleList(
+                [conv2d_block(chn_in, self.chn_out[i], self.kernel[i], self.strides[i])]
+            ))
+        
+        self.gru1 = nn.GRU(input_size=128, num_layers=2, hidden_size=64, 
+            batch_first = True, bidirectional=True)
+        self.gru2 = nn.GRU(input_size=7168, num_layers=2, hidden_size=128,
+            batch_first = True, bidirectional=True)
+
+        self.lin_depth = 2
+        in_feat, out_feat = 256, 256
+        self.lin_list = nn.ModuleList([])
+        for i in range(self.lin_depth):
+            self.lin_list.append(nn.ModuleList(
+                [linear_block(in_feat, out_feat)]
+            ))
+    
+    def forward(self, x):
+        # convert to log-freq log-mag stft 
+        x = torch.log(self.stft(x) + 1e-32)
+        # add channel dimension 
+        x = torch.unsqueeze(x, 1)
+
+        # 1. stack of 5 convolutional layer + relu
+        for i, module in enumerate(self.conv_list):
+            x = module[0](x)
+        
+        # 2. GRUs
+        x = torch.permute(x, (0, 3, 2, 1))
+        T = x.size(dim=1)
+        # TODO features: time and freq axis (my own interpretation because paper 
+        # is unclear about this step
+        x = torch.reshape(x, (-1, x.size(dim=1)*x.size(dim=2), x.size(dim=3)))
+        x, hn = self.gru1(x)
+        # features: freq and channel axis 
+        x = torch.reshape(x, (-1, T, 7168))
+        x, hn = self.gru2(x)
+
+        # 3. stack of 2 linear layaer + layernorm + relu
+        for i, module in enumerate(self.lin_list):
+            x = module[0](x)          
+        return x 
+            
+
+class conv2d_block(nn.Module):
+    def __init__(self, chn_in, chn_out, kernel, stride):
+        super().__init__()
+        ''' 2D convolutional layer + relu activation '''
+        self.conv2d = nn.Conv2d(chn_in, chn_out, kernel, stride)
+
+    def forward(self, x):
+        x = self.conv2d(x)
+        y = F.relu(x)
+        return y 
+
+class linear_block(nn.Module):
+    def __init__(self, in_feat, out_feat):
+        super().__init__()
+        ''' Linear + layerNorm + relu activation '''
+        self.linear = nn.Linear(in_feat, out_feat)
+        self.layer_norm = nn.LayerNorm(out_feat)
+
+    def forward(self, x):
+        x = self.linear(x)
+        x = self.layer_norm(x)
+        y = F.relu(x)
+        return y 
+
+class ProjectionLayer(nn.Module):
+    def __init__(self, in_feats, z1=1, z2=8, bias=None, activation=None):
+        super().__init__()
+
+        self.linear1 = nn.Linear(in_feats[0], z1)
+        self.linear2 = nn.Linear(in_feats[1], z2)
+        # initialize bias 
+        if bias is not None:
+            self.linear2.bias.data.fill_(0)
+            self.linear2.bias = nn.Parameter(bias)
+        else:
+            self.linear1.bias.data.fill_(0)
+            self.linear2.bias.data.fill_(0)
+        self.activation = activation
+
+    def forward(self, x):
+        x = torch.transpose(x, 2, 1)
+        x = self.linear1(x)
+        x = torch.transpose(x, 2, 1)
+        y = self.linear2(x)
+        # nonlinear activation
+        if self.activation is not None:
+            y = self.activation(y)
+        return y
+
+
+# ARP ESTIMATION NETWORK 
+class ASPestNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        
+        self.encoder = Encoder()
+        self.sigmoid = nn.Sigmoid()
+        z1, z2 = 1, 8
+        self.sr = 44100
+        omega_min, omega_max = 40, 12000 
+        
+        k = torch.arange(1,z2+1,1)
+        # initial cutoff freqs such that the freqs are equally spaced in the logarithmic scale 
+        omegaK = 2*(omega_min*(omega_max/omega_min)**((k-1)/(z2-1)))/self.sr 
+        # inverse of the sigmoid function 
+        bias_f = lambda x: torch.log(x/(1-x))
+
+        # TODO investigate on the weight initialization. using the default pytorch init 
+        # might shuffles the frequencies.
+        self.fC1ProjLayer = ProjectionLayer(
+            (78, 256), 1, 8, 
+            bias = bias_f(omegaK),
+            activation = lambda x: torch.tan(torch.pi * self.sigmoid(x)/2))
+            # activation = lambda x: torch.tan(torch.pi * self.sigmoid(
+            #   torch.tan(torch.pi * x / 44100 )) / 2))
+        self.fCdeltaProjLayer = ProjectionLayer(
+            (78, 256), 1, 8, 
+            bias = bias_f(omegaK),
+            activation = lambda x: torch.tan(torch.pi * self.sigmoid(x)/2))           
+
+        self.RC1ProjLayer = ProjectionLayer(
+            (78, 256), 1, 8,
+            activation = lambda x: torch.log(1+torch.exp(x)) / torch.log(torch.tensor(2,  device=get_device())))
+        bias = torch.ones((3, 8), device=get_device())
+        bias[1, :] = 2*torch.ones((1, 8), device=get_device())
+        self.mC1ProjLayer = ProjectionLayer(
+            (78, 256), 3, 8,
+            bias = bias)
+
+        self.GCdeltaProjLayer = ProjectionLayer(
+            (78, 256), 1, 8, 
+            bias = -10*torch.ones((z1, z2), device=get_device()), 
+            activation = lambda x: 10**(-torch.log(1+torch.exp(x)) / torch.log(torch.tensor(2,  device=get_device()))))
+        self.RCdeltaProjLayer = ProjectionLayer(
+            (78, 256), 1, 8,
+            activation = lambda x: torch.log(1+torch.exp(x))  / torch.log(torch.tensor(2,  device=get_device()))) 
+            
+
+        self.SAProjLayer = ProjectionLayer(
+            (78, 256), 6, 4, 
+            activation = self.sigmoid)
+        self.bcProjLayer = ProjectionLayer(
+            (78, 256), 2, 6)
+        self.hProjLayer = ProjectionLayer(
+            (78, 256), 1, 232)  # in the original paper it was 100 
+
+        # delay lengths
+        self.d = torch.tensor([233, 311, 421, 461, 587, 613],  device=get_device())
+        self.M = self.d.size(0)
+        self.Q0 = torch.tensor(householder(self.M), device=get_device())  # TODO: when to change Houlsehoöder matrix ? 
+
+        # all pass filter delay lengths
+        self.dAP = torch.tensor([
+            [131, 151, 337, 353], 
+            [103, 173, 331,373], 
+            [89, 181, 307, 401], 
+            [79, 197, 281, 419], 
+            [61, 211, 257, 431], 
+            [47, 229, 251, 443]],  device=get_device())   
+        # length of IR  
+        self.ir_length = int(2*self.sr)
+        
+        # normalization term 
+        self.bc_norm = nn.Parameter(torch.ones(2,6))
+        self.h0_norm = torch.ones((1,1), device=get_device())
+        self.ir_norm = torch.ones((1,1), device=get_device())
+
+    def forward(self, x, z):
+        bs = x.size(0)  # batch size
+        # STFT 
+        # x = torch.stft(x, n_fft=1024, hop_length=128, window=torch.hann_window(1024), return_complex = True)
+        # Model/Task-Agnostic Encoder
+        x = self.encoder(x) # out: [bs, 109, 256] or [bs, 78, 256]
+        # ARP-Groupwise Projection Laters
+        bc = self.bcProjLayer(x)
+        # bc = bc * self.bc_norm  # apply normalization term 
+        b, c = bc[:, 0, :], bc[:, 1, :]
+        b = torch.complex(b, torch.zeros(b.size(), device=get_device()))
+        c = torch.complex(c, torch.zeros(c.size(),  device=get_device()))
+        h0 = self.hProjLayer(x).squeeze(dim=1)
+        # common post filter
+        fC1 = self.fC1ProjLayer(x).squeeze(dim=1)
+        RC1 = self.RC1ProjLayer(x).squeeze(dim=1)
+        mC1 = self.mC1ProjLayer(x)
+        C1 = SVF(z, fC1, RC1, mC1[:, 0, :], mC1[:, 1, :], mC1[:, 2, :])
+        # common parallel delta-coloration filters 
+        fCdelta = self.fCdeltaProjLayer(x).squeeze(dim=1)
+        GCdelta = self.GCdeltaProjLayer(x).squeeze(dim=1)
+        RCdelta = self.RCdeltaProjLayer(x).squeeze(dim=1)
+        Cdelta = PEQ(z, fCdelta, RCdelta, GCdelta)
+        Cdelta = Cdelta.expand(self.M, -1, -1).permute(1, 2, 0)
+        gamma = self.SAProjLayer(x)
+        U = SAP(z, self.dAP, gamma)
+        # channel-wise allpass filters
+       
+        # delay matrix
+        D = torch.diag_embed(torch.unsqueeze(z, dim=-1)  ** (self.d)).expand(bs, -1 ,-1, -1)
+        # unitary matrix - Householder
+
+        Q0 = self.Q0
+        # Gamma = torch.diag(0.9999**self.d)
+        # TODO find why U creates resonances and balance energy of the ealry reflections
+        # H = torch.einsum('ik,ijkk->ijk', c, torch.inverse(D -  torch.diag_embed(Cdelta)*torch.matmul(Q0,Gamma)))
+        H = torch.einsum('ik,ijkk->ijk', c, torch.inverse(D - torch.diag_embed(U*Cdelta)*Q0 + 1e-16))
+        
+        H = C1*torch.einsum('ik,ijk->ij', b, H)
+        # channel-wise allpass filters 
+        ir_late =  torch.fft.irfft(H,  norm='ortho')
+        h0 = self.h0_norm.expand(bs, 1)*F.pad(h0, (0, self.ir_length-h0.size(dim=1)))
+        ir = self.ir_norm.expand(bs, 1)*(h0 + ir_late[:,:self.ir_length])
+        return ir, ir_late, h0
+    
+    def get_filters(self, x, z):
+        x = self.encoder(x) # out: [bs, 109, 256]
+        # get filters parameters and freuqency response as dictionary 
+        parameters = {}
+        # TODO parameters are missing 
+        parameters['fC1'] = self.fC1ProjLayer(x).squeeze(dim=1)
+        parameters['RC1'] = self.RC1ProjLayer(x).squeeze(dim=1)
+        parameters['mC1'] = self.mC1ProjLayer(x)
+
+        parameters['fCdelta'] = self.fCdeltaProjLayer(x).squeeze(dim=1)
+        parameters['GCdelta'] = self.GCdeltaProjLayer(x).squeeze(dim=1)
+        parameters['RCdelta'] = self.RCdeltaProjLayer(x).squeeze(dim=1)
+
+        parameters['gamma'] = self.SAProjLayer(x)
+
+        filters_tf = {}
+        filters_tf['C1'] = SVF(
+            z, 
+            parameters['fC1'], 
+            parameters['RC1'], 
+            parameters['mC1'][:, 0, :], 
+            parameters['mC1'][:, 1, :], 
+            parameters['mC1'][:, 2, :]).detach().numpy()
+        filters_tf['Cdelta'] = PEQ(
+            z, 
+            parameters['fCdelta'], 
+            parameters['GCdelta'], 
+            parameters['RCdelta']).detach().numpy()
+        filters_tf['U'] = SAP(
+            z, 
+            self.dAP, 
+            parameters['gamma']).detach().numpy()
+        
+        for param_key in parameters:
+            try:
+                parameters[param_key] = parameters[param_key].detach().numpy()
+            except:
+                continue
+        return parameters, filters_tf
